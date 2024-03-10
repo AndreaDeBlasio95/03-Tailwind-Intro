5e57a71b9dd6fd596bfaf1b8046e53c71fdc0d83
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-03-10 13:28:28 +0100

    playing with styles

--- react-tailwind-app/src/App.jsx
+++ react-tailwind-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <header className="flex flex-col items-center mt-8 mb-16">
         <h1 className="text-3xl font-bold mt-2">React & Tailwind</h1>
+        <Button>This is a button</Button>
       </header>
     </>
   );
