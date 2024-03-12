18970c759570de33a2217d1922104fa158b6cae5
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-03-12 11:39:39 +0100

    playing with tailwind

---
+++ react-tailwind-app/src/components/Container.jsx
@@ -0,0 +1,7 @@
+export default function Container() {
+  return (
+    <div>
+      <h1 className="">Hello World!</h1>
+    </div>
+  );
+}
--- react-tailwind-app/tailwind.config.js
+++ react-tailwind-app/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      0: "4px",
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#49e659",
+          932: "#434252",
+        },
+      },
+    },
   },
   plugins: [],
 };
